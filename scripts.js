4de6f3d9785bcb18a80e211231a2e4d25a563971
hanadrehman <hanad.rehman@healthifyme.com>
2021-11-20 23:18:11 +0530

    adds states

--- scripts.js
+++ scripts.js
@@ -4,10 +4,20 @@ function handleLogin(){
   if(username === 'jbmu45' && password === 'password'){
    document.querySelector('#login').style.display = 'none'
    document.querySelector('#info').style.display = 'block'
+   document.querySelector('#loginFailure').style.display = 'block';
+  } else {
+    document.querySelector('#loginFailure').style.display = 'block';
   }
 }
 
 function handleLogout(){
    document.querySelector('#login').style.display = 'block'
    document.querySelector('#info').style.display = 'none'
+   document.querySelector('#loginFailure').style.display = 'none';
+}
+
+function handlePasswordChange(ev){
+  if(ev.value.length>0){
+    document.querySelector('#loginBtn').disabled = false
+  }
 }
