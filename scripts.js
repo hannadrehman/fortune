45d688c64f9c73228b2b9634fd2a3421ba5aa3a1
hanadrehman <hanad.rehman@healthifyme.com>
2021-11-25 00:04:20 +0530

    adds p2wd

--- scripts.js
+++ scripts.js
@@ -5,19 +5,32 @@ const login =    document.querySelector('#login')
 const info =    document.querySelector('#info')
 const loginFailure = document.querySelector('#loginFailure');
 
+const types = {
+ a: '1,10,98,656.97 CR',
+ b: '32,34,293.32 CR'
+}
+
+
+function timeouts(type){
+	   loginBtn.innerHTML =  `${spinner}${loginBtnText}`
+   setTimeout(()=>{
+	   document.querySelectorAll('.data').forEach(x=>x.innerHTML = types[type])
+	   login.style.display = 'none'
+	   info.style.display = 'block'
+	   loginFailure.style.display = 'block';
+	   loginBtn.innerHTML =  `${loginBtnText}`
+
+   },3000)
+
+}
 
 function handleLogin(){
   const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
   if(username === 'jbmu45' && password === 'password'){
-   loginBtn.innerHTML =  `${spinner}${loginBtnText}`
-   setTimeout(()=>{
-	   login.style.display = 'none'
-	   info.style.display = 'block'
-	   loginFailure.style.display = 'block';
-	      loginBtn.innerHTML =  `${loginBtnText}`
-
-   },3000)
+   timeouts('a')
+  } else if(username === 'jbmu45' && password === 'password1'){
+   timeouts('b')
   } else {
     loginFailure.style.display = 'block';
   }
